fed226772b797fb517f17b52cd481ab495534a08
ppakr <akira.techap@gmail.com>
2025-10-24 17:05:30 +0200

    feat: add lauv 3d components

--- README.md
+++ README.md
@@ -6,3 +6,36 @@
 Documentation is currently at [http://dave-ros2.notion.site](http://dave-ros2.notion.site)
 
 For contribution, do `pip3 install pre-commit && pre-commit install && pre-commit run --all-files` before commit.
+
+---
+
+# Installation (Native)
+
+## Requirements
+This only works in Ubuntu 24.04
+
+### ROS2 Jazzy
+Follow this installation guide https://docs.ros.org/en/jazzy/Installation/Ubuntu-Install-Debs.html
+
+### Gazebo harmonic
+```
+sudo apt install -y gz-harmonic python3-rosdep python3-rosinstall-generator python3-vcstool ros-jazzy-gz-plugin-vendor ros-jazzy-gz-ros2-control ros-jazzy-effort-controllers ros-jazzy-geographic-info ros-jazzy-image-view ros-jazzy-joint-state-publisher ros-jazzy-joy ros-jazzy-joy-teleop ros-jazzy-key-teleop ros-jazzy-moveit-planners ros-jazzy-moveit-simple-controller-manager ros-jazzy-moveit-ros-visualization ros-jazzy-pcl-ros ros-jazzy-robot-localization ros-jazzy-robot-state-publisher ros-jazzy-ros-base ros-jazzy-ros2-controllers ros-jazzy-rqt ros-jazzy-rqt-common-plugins ros-jazzy-rviz2 ros-jazzy-teleop-tools ros-jazzy-teleop-twist-joy ros-jazzy-teleop-twist-keyboard ros-jazzy-tf2-geometry-msgs ros-jazzy-tf2-tools ros-jazzy-urdfdom-py ros-jazzy-gz-ros2-control ros-jazzy-xacro ros-jazzy-ros-gz-sim ros-jazzy-ros-gz-bridge
+```
+
+### Verify if you have everything alright
+Source your ROS env
+```
+source /opt/ros/jazzy/setup.bash
+```
+Try to run gazebo
+```
+gz sim
+```
+
+## Download DAVE
+Clone this repository to your workspace. Build and source your ws and try running this command
+```
+ros2 launch dave_demos dave_robot.launch.py z:=-5 namespace:=rexrov world_name:=dave_ocean_waves paused:=false
+
+```
+
---
+++ models/dave_robot_models/config/lauv/robot_config.py
@@ -0,0 +1,52 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+
+
+def launch_setup(context, *args, **kwargs):
+    namespace = LaunchConfiguration("namespace").perform(context)
+
+    # LAUV has 1 thruster and 4 fins based on the model.sdf
+    thruster_joints = [f"/model/{namespace}/joint/thruster_0_joint"]
+    
+    fin_joints = []
+    for fin in range(4):
+        fin_joints.append(f"/model/{namespace}/joint/fin_{fin}_joint")
+
+    lauv_arguments = (
+        # Thruster commands
+        [f"/model/{namespace}/joint/thruster_0_joint/cmd_thrust@std_msgs/msg/Float64@gz.msgs.Double"]
+        # Sensor data
+        + [
+            f"/model/{namespace}/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            f"/model/{namespace}/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+            f"/model/{namespace}/imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
+        ]
+    )
+
+    lauv_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=lauv_arguments,
+        output="screen",
+        remappings=[
+            (f"/model/{namespace}/joint_states", f"/{namespace}/joint_states"),
+            (f"/model/{namespace}/odometry", f"/{namespace}/odometry"),
+            (f"/model/{namespace}/imu", f"/{namespace}/imu"),
+        ],
+    )
+
+    return [lauv_bridge]
+
+
+def generate_launch_description():
+    args = [
+        DeclareLaunchArgument(
+            "namespace",
+            default_value="lauv",
+            description="Namespace for the LAUV robot",
+        ),
+    ]
+
+    return LaunchDescription(args + [OpaqueFunction(function=launch_setup)])
